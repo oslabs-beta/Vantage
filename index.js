ba19cf6f05db9663b40a6c29fb5c5383a3b31a77
Ari Shoham <arishoham@aris-mbp.lan>
2022-03-04 19:50:50 -0500

    can now write files into frostyjs folder

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ import chalk from "chalk";
 import { createSpinner } from "nanospinner";
 import inquirer from "inquirer";
 import chalkAnimation from "chalk-animation";
+import fs from "fs";
+import path from "path";
+
+// Get file directory
+// import { fileURLToPath } from 'url';
+// const __dirname = path.dirname(fileURLToPath(import.meta.url));
+// const currentDir = path.resolve(process.argv[1], "../../../");
+// const frostyjsDir = path.join(currentDir, "/frostyjs");
+const frostyjsDir = path.resolve('./frostyjs')
 
 const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
 
@@ -21,7 +30,7 @@ async function askRunFrosty() {
 
   // console.log(typeof answers.question_1);
   if (answers.question_1) {
-    return handleFrosty(answers.question_1);
+    return handleFrosty(process.argv[2]);
   } else console.log(chalk.red("frosty exited"));
 }
 
@@ -36,15 +45,30 @@ async function loading(success) {
 async function handleFrosty(success) {
   const spinner = createSpinner("Calculating web vitals").start();
   await sleep();
-  if (success) {
+  if (success !== "-n") {
     spinner.success({
-      text: `Frosty successful! run ${chalk.green("frosty analytics")} to see your web vitals`,
+      text: `Frosty successful! run ${chalk.green(
+        "frosty analytics"
+      )} to see your web vitals`,
     });
     process.exit(1);
   } else {
-    spinner.error({});
+    spinner.error({
+      text: `Frosty failed! try again with ${chalk.red("npx frostyjs")}`,
+    });
   }
 }
 
+function writeJSON() {
+  if (!fs.existsSync(frostyjsDir)) {
+    fs.mkdirSync(frostyjsDir);
+  }
+  fs.appendFile(frostyjsDir + "/data.json", "{}", (err) => {
+    if (err) console.log(err);
+  });
+}
+
 // await test();
-export default await askRunFrosty();
+writeJSON();
+console.log(frostyjsDir)
+// askRunFrosty();
