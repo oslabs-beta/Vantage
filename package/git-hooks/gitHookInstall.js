5d0d791fb06415dfd7370f914eb4202ea7dd67ca
Ari Shoham <arishoham@gmail.com>
2022-03-07 19:22:08 -0500

    fixed file structure to comply with new standard

--- package/git-hooks/gitHookInstall.js
+++ package/git-hooks/gitHookInstall.js
@@ -1,31 +1,31 @@
-import fs from "fs";
-import { resolve } from "path";
-import chalk from "chalk";
+import fs from 'fs';
+import { resolve } from 'path';
+import chalk from 'chalk';
 
-const hook = "post-commit";
+const hook = 'post-commit';
 //Get files of hook template
 const hookFileContents = fs.readFileSync(`./${hook}`).toString();
 
 function installHooks() {
   //Make sure there is a .git folder
-  const gitRoot = resolve(process.cwd() + "/.git");
+  const gitRoot = resolve(process.cwd() + '/.git');
   if (fs.existsSync(gitRoot)) {
-    const hooksDir = resolve(gitRoot, "hooks");
+    const hooksDir = resolve(gitRoot, 'hooks');
     ensureDirExists(hooksDir); //Add hooks folder if it doesn't exist
     const hookFile = resolve(hooksDir, hook);
     if (fs.existsSync(hookFile)) {
       console.warn(
         chalk.red(
-          "Post-commit git hook already exists.\nRefer to [online docs] about adding Frosty commit integration"
+          'Post-commit git hook already exists.\nRefer to [online docs] about adding Frosty commit integration'
         )
       );
       return;
     }
     fs.writeFileSync(hookFile, hookFileContents); //create hook file
-    fs.chmodSync(hookFile, "755"); //make hook file executable
-    console.log(chalk.green("Frosty git integration was successful!"));
+    fs.chmodSync(hookFile, '755'); //make hook file executable
+    console.log(chalk.green('Frosty git integration was successful!'));
   } else {
-    console.warn("This does not seem to be a git project.");
+    console.warn('This does not seem to be a git project.');
   }
 }
 
--- package/git-hooks/post-commit-js.js
+++ package/git-hooks/post-commit-js.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
-var childProcess = require("child_process");
+var childProcess = require('child_process');
 
 //Get commit message
 const commitMsg = childProcess
-  .execSync("git log -1 --pretty=%B")
+  .execSync('git log -1 --pretty=%B')
   .toString()
   .trim();
 console.log(commitMsg);
 
 //execute frosty with message
-childProcess.exec("frosty");
+childProcess.exec('frosty');
 process.exit(1);
