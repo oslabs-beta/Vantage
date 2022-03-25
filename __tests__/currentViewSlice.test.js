e2a97f199a8bef9744d1ec6170487a0f07ae581c
Paul Perez <pau.per92@gmail.com>
2022-03-25 16:58:22 -0400

    testing default state of currentViewSlice.js

--- __tests__/currentViewSlice.test.js
+++ __tests__/currentViewSlice.test.js
@@ -1,1 +1,35 @@
 import { createSlice } from "@reduxjs/toolkit";
+import "@testing-library/jest-dom";
+import { currentViewSlice,  } from '../store/currentViewSlice';
+
+
+describe('CurrentViewSlice Testing', () => {
+  let initialState;
+
+  beforeEach(() => {
+    initialState = {
+      theme: "dark",
+      currentMetric: "default",
+      currentEndpoint: "/",
+      performanceMetrics: {
+        FCP: false,
+        SI: false,
+        LCP: false,
+        TTI: false,
+        TBT: false,
+        CLS: false,
+      },
+      runValueArr: [],
+      runValueArrSort: [],
+      selectorSwitch: false,
+    };
+  });
+
+  describe('default state', () => {
+    it('should return a default state when given an undefined input', () => {
+      expect(subject(undefined, { type: undefined })).toEqual(state);
+    });
+  });
+
+
+});
