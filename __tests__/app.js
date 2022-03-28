5d928959485c73be2b6cefbbd877f4dc69b2d73b
Ari Shoham <arishoham@gmail.com>
2022-03-28 13:03:58 -0400

    added tests for suggestions

--- __tests__/app.js
+++ __tests__/app.js
@@ -5,20 +5,13 @@
 import React from "React";
 import { Provider } from "react-redux";
 import store from "../client/store/store";
-import {
-  render,
-  screen,
-  waitFor,
-  fireEvent,
-  prettyDOM,
-  within,
-} from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import App from "../client/App.jsx";
 import "@testing-library/jest-dom";
 import regeneratorRuntime from "regenerator-runtime";
 
 describe("React-Redux integration tests", () => {
-  describe("Empty state before interactions", () => {
+  describe("Render app before each test", () => {
     let app;
     beforeEach(async () => {
       process.env.NODE_ENV = "development";
@@ -86,7 +79,7 @@ describe("React-Redux integration tests", () => {
       });
     });
 
-    describe("Checking performance web vitals", () => {
+    describe("Performance web vitals", () => {
       beforeEach(() => {
         //Close suggestions
         const curEndpoint = app.getByText("Current Endpoint:");
@@ -155,7 +148,7 @@ describe("React-Redux integration tests", () => {
       });
     });
 
-    describe("Testing chart range switch", () => {
+    describe("Chart range switch", () => {
       let switchContainer;
       beforeEach(() => {
         const metric = app.getAllByText("Performance")[0];
@@ -174,6 +167,58 @@ describe("React-Redux integration tests", () => {
       test("selectorSwitch in to be true after range switch is clicked", () => {
         fireEvent.click(app.container.querySelector("#range-switch-click"));
         expect(store.getState().currentView.selectorSwitch).toBe(true);
+      });
+    });
+
+    describe("Suggestions", () => {
+      const metricArr = [];
+      beforeEach(() => {
+        metricArr[0] = app.getAllByText("Performance")[0];
+        metricArr[1] = app.getAllByText("Accessibility")[0];
+        metricArr[2] = app.getAllByText("Best Practices")[0];
+        metricArr[3] = app.getAllByText("SEO")[0];
+      });
+
+      test("Each Suggestion has two paragraphs and a button", () => {
+        metricArr.forEach((metric) => {
+          fireEvent.click(metric);
+          const suggestions = app.container.querySelectorAll(".suggestion");
+          suggestions.forEach((cur) => {
+            expect(cur).toBeInTheDocument();
+            expect(cur.children[0].nodeName).toBe("P");
+            expect(cur.children[1].nodeName).toBe("P");
+            expect(cur.lastChild.nodeName).toBe("BUTTON");
+          });
+        });
+      });
+
+      test("Each suggestion description has text", () => {
+        metricArr.forEach((metric) => {
+          fireEvent.click(metric);
+          const suggestions = app.container.querySelectorAll(".suggestion");
+          suggestions.forEach((cur) => {
+            expect(cur.children[0].innerHTML).toBeTruthy();
+          });
+        });
+      });
+
+      test("Each suggestion has a valid value", () => {
+        metricArr.forEach((metric) => {
+          fireEvent.click(metric);
+          const suggestions = app.container.querySelectorAll(".suggestion");
+          suggestions.forEach((cur) => {
+            const num = Number(cur.children[1].innerHTML);
+            if (isNaN(num)) {
+              expect(isNaN(cur.children[1].firstChild.nodeValue)).toBe(false);
+              expect(cur.children[1].children[0].innerHTML).toMatch(
+                /ms|B|elements|s|KiB|Kelements/
+              );
+            } else {
+              expect(num).toBeLessThanOrEqual(100);
+              expect(num).toBeGreaterThanOrEqual(0);
+            }
+          });
+        });
       });
     });
   });
